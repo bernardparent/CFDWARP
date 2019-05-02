71aff22806c740695884c395ee5e363fab84d6df
Bernard Parent <bernparent@gmail.com>
2019-05-02 10:13:23 +0900

    19may02a

--- src/version.hh
+++ src/version.hh
@@ -1,1 +1,1 @@
-#define VERSION "19apr29a"
+#define VERSION "19may02a"
--- tools/tauvt.c
+++ tools/tauvt.c
@@ -1,3 +1,17 @@
+// SPDX-License-Identifier: BSD-2-Clause
+/*
+Copyright 2014 Sathyan Padmanabhan
+
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+
 /* Vibrational-Translational (V-T) relaxation time of N2 for N2-O2 mixture's:
    Millikan and White gave an empirical correlation for obtaining the
    V-T relaxation based on molecular collision's[1]. It can be applied to 
