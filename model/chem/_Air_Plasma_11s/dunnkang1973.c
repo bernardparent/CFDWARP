0e93f546e6edb5284f1f3f4c928a2114fff14700
Bernard Parent <bernparent@gmail.com>
2021-01-19 13:58:34 -0700

    21jan19a

--- model/chem/_Air_Plasma_11s/dunnkang1973.c
+++ model/chem/_Air_Plasma_11s/dunnkang1973.c
@@ -105,19 +105,23 @@ void find_W_DunnKang1973 ( gl_t *gl, spec_t rhok, double T, double Te, double Tv
   if (REACTION[10])
     add_to_W_fwbw_2r3p ( specN, specN2, specN, specN, specN, 4.085E22, -1.5, 226000.0, T, X, W );
 
-  if (REACTION[11])
-    add_to_W_fwbw_2r2p ( specO, specN, specNOplus, speceminus, 1.4E6, 1.5, 63800.0, T, X, W );
+  if (REACTION[11]){
+    add_to_W_fw_2r2p ( specO, specN, specNOplus, speceminus, 1.4E6, 1.5, 63800.0, T, X, W );
+    add_to_W_bw_2r2p ( specO, specN, specNOplus, speceminus, 1.4E6, 1.5, 63800.0, Te, X, W );
+  }
 
   if (REACTION[12])
     add_to_W_fwbw_2r3p ( specO, speceminus,
-                             specOplus, speceminus, speceminus, 3.6E31, -2.91, 316000.0, T, X, W );
+                             specOplus, speceminus, speceminus, 3.6E31, -2.91, 316000.0, Te, X, W );
 
   if (REACTION[13])
     add_to_W_fwbw_2r3p ( specN, speceminus,
-                             specNplus, speceminus, speceminus, 1.1E32, -3.14, 338000.0, T, X, W );
+                             specNplus, speceminus, speceminus, 1.1E32, -3.14, 338000.0, Te, X, W );
 
-  if (REACTION[14])
-    add_to_W_fwbw_2r2p ( specO, specO, specO2plus, speceminus, 1.6E17, -0.98, 161600.0, T, X, W );
+  if (REACTION[14]){
+    add_to_W_fw_2r2p ( specO, specO, specO2plus, speceminus, 1.6E17, -0.98, 161600.0, T, X, W );
+    add_to_W_bw_2r2p ( specO, specO, specO2plus, speceminus, 1.6E17, -0.98, 161600.0, Te, X, W );
+  }
 
   if (REACTION[15])
     add_to_W_fwbw_2r2p ( specO, specO2plus, specO2, specOplus, 2.92E18, -1.11, 56000.0, T, X, W );
@@ -125,8 +129,10 @@ void find_W_DunnKang1973 ( gl_t *gl, spec_t rhok, double T, double Te, double Tv
   if (REACTION[16])
     add_to_W_fwbw_2r2p ( specN2, specNplus, specN, specN2plus, 2.02e11, 0.81, 26000.0, T, X, W );
 
-  if (REACTION[17])
-    add_to_W_fwbw_2r2p ( specN, specN, specN2plus, speceminus, 1.4e13, 0.0, 135600.0, T, X, W );
+  if (REACTION[17]){
+    add_to_W_fw_2r2p ( specN, specN, specN2plus, speceminus, 1.4e13, 0.0, 135600.0, T, X, W );
+    add_to_W_bw_2r2p ( specN, specN, specN2plus, speceminus, 1.4e13, 0.0, 135600.0, Te, X, W );
+  }
 
   if (REACTION[18])
     add_to_W_fwbw_2r2p ( specO, specNOplus, specNO, specOplus, 3.63E15, -0.6, 101600.0, T, X, W );
@@ -164,12 +170,19 @@ void find_W_DunnKang1973 ( gl_t *gl, spec_t rhok, double T, double Te, double Tv
   if (REACTION[29])
     add_to_W_fwbw_2r3p ( specNO, specNO, specN, specO, specNO, 7.8E20, -1.5, 151000.0, T, X, W );
 
-  if (REACTION[30])
-    add_to_W_fwbw_2r3p ( specO2, specN2,
+  if (REACTION[30]){
+    add_to_W_fw_2r3p ( specO2, specN2,
                              specNO, specNOplus, speceminus, 1.38E20, -1.84, 282000.0, T, X, W );
-  if (REACTION[31])
-    add_to_W_fwbw_2r3p ( specNO, specN2,
+    add_to_W_bw_2r3p ( specO2, specN2,
+                             specNO, specNOplus, speceminus, 1.38E20, -1.84, 282000.0, Te, X, W );
+  }
+
+  if (REACTION[31]){
+    add_to_W_fw_2r3p ( specNO, specN2,
                              specNOplus, speceminus, specN2, 2.2E15, -0.35, 216000.0, T, X, W );
+    add_to_W_bw_2r3p ( specNO, specN2,
+                             specNOplus, speceminus, specN2, 2.2E15, -0.35, 216000.0, Te, X, W );
+  }
 }
 
 
@@ -233,23 +246,29 @@ void find_dW_dx_DunnKang1973 ( gl_t *gl, spec_t rhok, spec_t mu, double T, doubl
     add_to_dW_fwbw_2r3p ( specN, specN2,
                                specN, specN, specN, 4.085E22, -1.5, 226000.0, T, X, dWdT, dWdrhok );
 
-  if (REACTION[11]) 
-    add_to_dW_fwbw_2r2p ( specO, specN,
+  if (REACTION[11]) {
+    add_to_dW_fw_2r2p ( specO, specN,
                                specNOplus, speceminus, 1.4E6, 1.5, 63800.0, T, X, dWdT, dWdrhok );
+    add_to_dW_bw_2r2p ( specO, specN,
+                               specNOplus, speceminus, 1.4E6, 1.5, 63800.0, Te, X, dWdTe, dWdrhok );
+  }
 
   if (REACTION[12]) 
     add_to_dW_fwbw_2r3p ( specO, speceminus,
                                specOplus, speceminus, speceminus,
-                               3.6E31, -2.91, 316000.0, T, X, dWdT, dWdrhok );
+                               3.6E31, -2.91, 316000.0, Te, X, dWdTe, dWdrhok );
 
   if (REACTION[13]) 
     add_to_dW_fwbw_2r3p ( specN, speceminus,
                                specNplus, speceminus, speceminus,
-                               1.1E32, -3.14, 338000.0, T, X, dWdT, dWdrhok );
+                               1.1E32, -3.14, 338000.0, Te, X, dWdTe, dWdrhok );
 
-  if (REACTION[14]) 
-    add_to_dW_fwbw_2r2p ( specO, specO,
+  if (REACTION[14]) {
+    add_to_dW_fw_2r2p ( specO, specO,
                                specO2plus, speceminus, 1.6E17, -0.98, 161600.0, T, X, dWdT, dWdrhok );
+    add_to_dW_bw_2r2p ( specO, specO,
+                               specO2plus, speceminus, 1.6E17, -0.98, 161600.0, Te, X, dWdTe, dWdrhok );
+  }
 
   if (REACTION[15]) 
     add_to_dW_fwbw_2r2p ( specO, specO2plus,
@@ -259,9 +278,12 @@ void find_dW_dx_DunnKang1973 ( gl_t *gl, spec_t rhok, spec_t mu, double T, doubl
     add_to_dW_fwbw_2r2p ( specN2, specNplus,
                                specN, specN2plus, 2.02e11, 0.81, 26000.0, T, X, dWdT, dWdrhok );
 
-  if (REACTION[17]) 
-    add_to_dW_fwbw_2r2p ( specN, specN,
+  if (REACTION[17]) {
+    add_to_dW_fw_2r2p ( specN, specN,
                                specN2plus, speceminus, 1.4e13, 0.0, 135600.0, T, X, dWdT, dWdrhok );
+    add_to_dW_bw_2r2p ( specN, specN,
+                               specN2plus, speceminus, 1.4e13, 0.0, 135600.0, Te, X, dWdTe, dWdrhok );
+  }
 
   if (REACTION[18]) 
     add_to_dW_fwbw_2r2p ( specO, specNOplus,
@@ -311,13 +333,21 @@ void find_dW_dx_DunnKang1973 ( gl_t *gl, spec_t rhok, spec_t mu, double T, doubl
     add_to_dW_fwbw_2r3p ( specNO, specNO,
                                specN, specO, specNO, 7.8E20, -1.5, 151000.0, T, X, dWdT, dWdrhok );
 
-  if (REACTION[30]) 
-    add_to_dW_fwbw_2r3p ( specO2, specN2,
+  if (REACTION[30]) {
+    add_to_dW_fw_2r3p ( specO2, specN2,
                                specNO, specNOplus, speceminus,
                                1.38E20, -1.84, 282000.0, T, X, dWdT, dWdrhok );
+    add_to_dW_bw_2r3p ( specO2, specN2,
+                               specNO, specNOplus, speceminus,
+                               1.38E20, -1.84, 282000.0, Te, X, dWdTe, dWdrhok );
+  }
 
-  if (REACTION[31]) 
-    add_to_dW_fwbw_2r3p ( specNO, specN2,
+  if (REACTION[31]) {
+    add_to_dW_fw_2r3p ( specNO, specN2,
                                specNOplus, speceminus, specN2,
                                2.2E15, -0.35, 216000.0, T, X, dWdT, dWdrhok );
+    add_to_dW_bw_2r3p ( specNO, specN2,
+                               specNOplus, speceminus, specN2,
+                               2.2E15, -0.35, 216000.0, Te, X, dWdTe, dWdrhok );
+  }
 }
